801f155e52d6461f92480f6ff831e29751654a48
Xz1128 <1393570272@qq.com>
2026-04-24 12:34:22 +0800

    Initial submission of code files for privacy pages (including HTML, CSS, TS).

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -1,20 +1,36 @@
 import { Component, OnInit } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonButton, IonLabel } from '@ionic/angular/standalone';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab4',
-  templateUrl: './tab4.page.html',
-  styleUrls: ['./tab4.page.scss'],
+  templateUrl: 'tab4.page.html',
+  styleUrls: ['tab4.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardContent,
+    IonButton,
+    IonLabel
+  ],
 })
 export class Tab4Page implements OnInit {
+  constructor(private alertController: AlertController) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  async showHelp() {
+    const alert = await this.alertController.create({
+      header: 'Help',
+      message: 'This page displays the complete Privacy & Security Policy of the inventory management application.',
+      buttons: ['OK']
+    });
+    await alert.present();
   }
-
 }
